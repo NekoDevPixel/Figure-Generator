17439186a1e1766936b2cf88b71bf130f4884b7d
NekoDevPixel <sahwang04@gmail.com>
2025-09-03 11:49:48 +0900

    도형을 생성하는 버튼 UI 제작

---
+++ Assets/Script/MoveSP.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MoveSP : MonoBehaviour
+{
+    private Vector2 mousePos;
+    private GameObject selectobject;
+    private bool mvchck = false;
+    Collider2D hit;
+
+    void Update()
+    {
+        CheckCollider();
+    }
+
+    void FixedUpdate()
+    {
+        moveShape();
+    }
+
+    void CheckCollider()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.OverlapPoint(mousePos);
+
+            if (hit != null)
+            {
+                selectobject = hit.gameObject;
+                mvchck = !mvchck;
+            }
+            else
+            {
+                selectobject = null;
+                mvchck = false;
+            }
+        }
+    }
+
+    void moveShape()
+    {
+        if (mvchck)
+        {
+            selectobject.transform.position = mousePos;
+        }
+    }
+}
